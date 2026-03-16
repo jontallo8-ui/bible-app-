7efe27fafcb87a60b42edd203dcd2d0053a394ea
jontallo8-ui <jontallo8@gmail.com>
2026-03-16 17:10:44 +0100

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../styles/components/Header.css'
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1>📖 Bible Study Application</h1>
+        <p className="tagline">Complete Bible with Dictionary, Concordance & Commentary</p>
+      </div>
+    </header>
+  )
+}
